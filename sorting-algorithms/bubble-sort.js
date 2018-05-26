ad65922a81c3d472815227e15f9636a5b0607acd
amgranad <ambergkim@gmail.com>
2018-05-26 13:21:27 -0700

    mvp

--- sorting-algorithms/bubble-sort.js
+++ sorting-algorithms/bubble-sort.js
@@ -1,3 +1,22 @@
+'use strict';
+
 module.exports = function(arr) {
+  let isSorted = false;
+
+  while (!isSorted) {
+    let foundOutofOrder = false;
+    for (let i = 0; i < arr.length; i++) {
+      let first = arr[i];
+      let second = arr[i + 1];
+      if (second < first) {
+        arr[i] = second;
+        arr[i + 1] = first;
+        foundOutofOrder = true;
+      }
+    }
+    if (foundOutofOrder === false) {
+      isSorted = true;
+    }
+  }
   return arr;
-}
+};
--- sorting-algorithms/insertion-sort.js
+++ sorting-algorithms/insertion-sort.js
@@ -1,3 +1,18 @@
+'use strict';
+
 module.exports = function(arr) {
-  return arr;
-}
+  let copiedArr = arr.splice();
+  let newArr = [];
+
+  while (newArr.length < arr.length) {
+    let smallest = copiedArr[0];
+    for (let i = 1; i < copiedArr.length; i++) {
+      if (copiedArr[i] < smallest) {
+        smallest = copiedArr[i];
+      }
+    }
+    newArr.push(smallest);
+  }
+
+  return newArr;
+};
