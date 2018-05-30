21245db942b30835c57c935e337851241e1aedd9
rjtj2007 <teacherman1@gmail.com>
2018-05-29 20:53:09 -0700

    quick-sort working properly

--- sorting-algorithms/quicksort.js
+++ sorting-algorithms/quicksort.js
@@ -1,3 +1,13 @@
-module.exports = function(arr) {
-  return arr;
-}
+module.exports = function quicksort(arr) {
+  if (arr.length <= 1) return arr;
+
+  var pivot = arr[0];
+  var left = []; 
+  var right = [];
+
+  for (var i = 1; i < arr.length; i++) {
+    arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i]);
+  }
+
+  return quicksort(left).concat(pivot, quicksort(right));
+};
