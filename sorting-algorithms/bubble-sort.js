0c468499ebe5ac8603b2e25f6bfc89271bc1ece9
Daniel Pillay <dpillay03@gmail.com>
2018-05-31 22:31:08 -0700

    bubble sort passing and complete

--- sorting-algorithms/bubble-sort.js
+++ sorting-algorithms/bubble-sort.js
@@ -1,3 +1,18 @@
 module.exports = function(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = 1; j < arr.length; j++) {
+      if(arr[j - 1] > arr[j]) {
+        let temp = arr[j-1];
+           arr[j-1] = arr[j];
+           arr[j] = temp;
+      }
+    }
+  }
   return arr;
 }
+
+
+//1. Loop through the array for [i]
+//2. Loop through the array for [j]
+//3. Compare if arr[j - 1] is greater than arr[i]
+//4. If it is greater, swap 
