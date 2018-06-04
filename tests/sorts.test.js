03977ad95fb7c5abc8c6a7c9d5de87ad18527bb3
ixnp <enprocopios@gmail.com>
2018-06-03 22:06:55 -0700

    quick not working

--- tests/sorts.test.js
+++ tests/sorts.test.js
@@ -14,8 +14,8 @@ describe("Sorting Algorithms", () => {
   // uncomment these as you start working on them.
   // testWithAlg("Bucket Sort", Sorters.BucketSort);
   // testWithAlg("Insertion Sort", Sorters.InsertionSort);
-  testWithAlg("Merge Sort", Sorters.MergeSort);
-  // testWithAlg("Quicksort", Sorters.Quicksort);
+  // testWithAlg("Merge Sort", Sorters.MergeSort);
+  testWithAlg("Quicksort", Sorters.Quicksort);
 });
 
 function testWithAlg(name, alg) {
