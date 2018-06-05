178d0cb82476373c891b447ee253a0bd63b31ebb
AaronBruce <aaron.bruce@comcast.net>
2018-06-04 22:14:25 -0700

    finished lab 40

--- sorting-algorithms/bubble-sort.js
+++ sorting-algorithms/bubble-sort.js
@@ -1,3 +1,14 @@
+'use strict';
+
 module.exports = function(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let x = 1; x < arr.length; x++) {
+      if (arr[x - 1] > arr[x]) {
+        let thing = arr[x - 1];
+        arr[x - 1] = arr[x];
+        arr[x] = thing;
+      }
+    }
+  };
   return arr;
-}
+};
--- sorting-algorithms/insertion-sort.js
+++ sorting-algorithms/insertion-sort.js
@@ -1,3 +1,14 @@
+'use strict';
+
 module.exports = function(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let element = arr[i];
+    let x = (i - 1);
+    while (x >= 0 && arr[x] > element) {
+      arr[x + 1] = arr[x];
+      x--;
+    }
+    arr[x + 1] = element;
+  }
   return arr;
-}
+};
